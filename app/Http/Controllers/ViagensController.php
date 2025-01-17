ccde279df9342d07798676fcc2aec7b6c78b4869
vinibalbino <silvavinicius2018@gmail.com>
2025-01-17 06:42:27 -0400

    feat: add viagens crud

--- app/Http/Controllers/ViagensController.php
+++ app/Http/Controllers/ViagensController.php
@@ -9,14 +9,50 @@ class ViagensController extends Controller
 {
     public function index()
     {
-        $viagens = Viagem::all();
-
+        $viagens = Viagem::with('motoristas', 'veiculo')->get();
         return view('viagens.index', ["viagens" => $viagens]);
     }
 
-    public function show($id) {}
+    public function show($id)
+    {
 
-    public function store() {}
+        $viagem = Viagem::findOrFail($id);
 
-    public function create() {}
+        return view($viagem->load('motoristas', 'veiculo'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'veiculo_id' => 'required|exists:veiculos,id',
+            'km_inicial' => 'required|integer|min:0',
+            'km_final' => 'required|integer|gt:km_inicial',
+            'motoristas' => 'required|array',
+            'motoristas.*' => 'exists:motoristas,id',
+        ]);
+
+        $viagem = Viagem::create([
+            'veiculo_id' => $validated['veiculo_id'],
+            'km_inicial' => $validated['km_inicial'],
+            'km_final' => $validated['km_final'],
+        ]);
+
+        $viagem->motoristas()->attach($validated['motoristas']);
+
+        // retorna a pagina visualizar
+    }
+
+    public function create()
+    {
+        // retorna pagina para criar viagem
+    }
+
+    public function destroy($id)
+    {
+        $viagem = Viagem::findOrFail($id);
+
+        $viagem->motoristas()->detach();
+        $viagem->delete();
+    }
 }
