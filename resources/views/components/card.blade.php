f5837d3ed7bb364e6adb3b9492b52d65041d06af
vinibalbino <silvavinicius2018@gmail.com>
2025-01-17 19:20:14 -0400

    refactor: Align button text on card layout

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,9 @@
 <div class="flex-auto m-5 max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <h5 class="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $attributes->get('heeader-card') }}</h5>
     {{ $slot }}
-    <div class="flex flex-wrap p-0 ">
+
+    <div class="flex flex-wrap p-0 text-center">
+
         @if ($attributes->get('details'))
         <a href="{{ $attributes->get('href') }}" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             <button type="button">
